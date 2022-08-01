65b53712642af1e15d0a25593ab54a97a94b5a08
ledouxm <mledoux@mledoux.fr>
2022-08-01 17:20:11 +0200

    feat: without prisma

--- prisma/migrations/20220128094015_initial/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Script" (
-    "id" TEXT NOT NULL,
-    "script" TEXT NOT NULL,
-
-    CONSTRAINT "Script_pkey" PRIMARY KEY ("id")
-);
--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { makeRouter } from "@/app";
-import { db } from "@/db";
 import { makeDebug } from "@/utils";
 
 const debug = makeDebug("server.ts");
 
 const main = async () => {
-    try {
         const app = makeRouter();
-        const results = await db.script.findMany();
-        debug("results", results);
-    } catch (e) {
-        throw e;
-    } finally {
-        await db.$disconnect();
-    }
 };
 
 main();
